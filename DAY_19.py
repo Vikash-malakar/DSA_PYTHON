5c8e54d25f558d33a02d6a977535fce6e27c81f7
Vikash Malakar <vikashmalakar312@gmail.com>
2026-07-18 21:50:56 +0530

    Day4/learn_DSA/triplet

---
+++ DAY_19.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first = float('inf')
+        second = float('inf')
+        
+        for n in nums:
+            if n <= first:
+                first = n
+            elif n <= second:
+                second = n
+            else:
+                return True
+                
+        return False
+
+if __name__ == "__main__":
+    sol = Solution()
+    
+    nums1 = [1, 2, 3, 4, 5]
+    print(sol.increasingTriplet(nums1))
+    
+    nums2 = [5, 4, 3, 2, 1]
+    print(sol.increasingTriplet(nums2))
+    
+    nums3 = [2, 1, 5, 0, 4, 6]
+    print(sol.increasingTriplet(nums3))
